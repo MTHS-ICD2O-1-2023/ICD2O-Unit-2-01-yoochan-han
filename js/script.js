d44114be799f9d7c0975b0287348b2bfe658e147
yoochan han <159185007+yoochan-han@users.noreply.github.com>
2024-03-05 19:47:14 +0000

    add js, css and html files

--- js/script.js
+++ js/script.js
@@ -1,5 +1,3 @@
-// Copyright (c) 2020 Mr. Coxall All rights reserved
-//
-// Created by: Mr. Coxall
-// Created on: Sep 2020
-// This file contains the JS functions for index.html
+function myButtonClicked() {
+  document.getElementById("hello-world").innerHTML = "<p>HelloWorld!</p>";
+}
