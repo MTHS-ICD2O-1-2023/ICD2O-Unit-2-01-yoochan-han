5290aa01bc59e7b812a030b99b5d28e2337341fe
yoochan han <159185007+yoochan-han@users.noreply.github.com>
2024-03-18 22:13:15 +0000

    debugged html, css, js

--- js/script.js
+++ js/script.js
@@ -1,3 +1,4 @@
+// This file contains the JS functions for index.html
 function myButtonClicked() {
-  document.getElementById("hello-world").innerHTML = "<p>HelloWorld!</p>";
+  document.getElementById("hello-world").innerHTML = "<p >Hello, World!</p>";
 }
