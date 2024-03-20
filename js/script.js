87ed1eb3034bc44c52f6a25e99d8008a13ae0d93
yoochan han <159185007+yoochan-han@users.noreply.github.com>
2024-03-20 23:27:48 +0000

    added html, css, JS

--- js/script.js
+++ js/script.js
@@ -1,4 +1,6 @@
+// Created by: Yoochan Han
+// Created on: Mar 2024
 // This file contains the JS functions for index.html
 function myButtonClicked() {
-  document.getElementById("hello-world").innerHTML = "<p >Hello, World!</p>";
+  document.getElementById("hello-world").innerHTML = "<p>Hello, World!</p>";
 }
